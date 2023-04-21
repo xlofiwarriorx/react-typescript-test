1882ce99c4fca9b1a9467f9c7e68c64eede6f18c
dmitrij <dmitrijstahovic@gmail.com>
2023-04-21 09:45:16 +0400

    first commit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage () {
+	return (
+		<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum facere enim sed neque magni error distinctio ex. Dolore, aut distinctio expedita mollitia, tempore accusantium quo aliquid fugit saepe repudiandae nemo optio ipsum odio fugiat cumque nesciunt modi reiciendis qui laborum illo voluptatibus alias corrupti odit? Expedita quis illo recusandae deserunt?</div>
+	)
+}
